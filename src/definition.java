a27046a777e0722f66f7a59ab12d0af24b48da3a
palash0000 <palash.goyal_cs18gla.ac.in>
2020-02-24 22:26:40 +0530

    all variable created

--- src/definition.java
+++ src/definition.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 public class definition {
-    void
+    public void addMobileNumber(){
+        ArrayList<String> numbers = new ArrayList<>();
+        String firstName=" ";
+        String lastName= " ";
+        String email =" ";
+        String phoneNumber = " ";
+    }
 }
